89ab707a0eb8be1e3a1e4d502c6a80a384df2eb0
Muhamad Taopik <mr.94t3z@gmail.com>
2024-06-15 13:44:13 +0700

    🐸 refactor code

--- api/index.tsx
+++ api/index.tsx
@@ -1,7 +1,7 @@
 import { Button, Frog, TextInput } from 'frog'
 import { handle } from 'frog/vercel'
 import { neynar } from 'frog/middlewares'
-import { Box, Divider, Image, Heading, Text, VStack, Spacer, vars } from "../lib/ui.js";
+import { Box, Divider, Heading, Text, VStack, Spacer, vars } from "../lib/ui.js";
 import dotenv from 'dotenv';
 
 // Uncomment this packages to tested on local server
@@ -39,108 +39,124 @@ const baseUrlNeynarV2 = process.env.BASE_URL_NEYNAR_V2;
 
 
 app.frame('/', async (c) => {
-  const { fid } = c.var.interactor || {}
-
-  try {
-    const response = await fetch(`${baseUrlNeynarV2}/user/bulk?fids=${fid}`, {
-      method: 'GET',
-      headers: {
-        'accept': 'application/json',
-        'api_key': process.env.NEYNAR_API_KEY || '',
-      },
-    });
-
-    const data = await response.json();
-    const userData = data.users[0];
-
-    return c.res({
-      title: 'Regen Stats',
-      image: (
-        <Box
-            grow
-            alignVertical="center"
-            backgroundColor="bg"
-            padding="48"
-            textAlign="center"
-            height="100%"
-            // border="1em solid rgb(254,253,251) "
-            // borderRadius="22"
-          >
-            <VStack gap="4">
-                {/* <Divider color="red" /> */}
-                {/* <Spacer size="22" /> */}
-                <Heading color="red" weight="900" align="center" size="64">
-                  REGEN 🍄 STATS
-                </Heading>
-                {/* <Spacer size="22" /> */}
+  return c.res({
+    title: 'Regen Stats',
+    image: (
+      <Box
+          grow
+          alignVertical="center"
+          backgroundColor="bg"
+          padding="48"
+          textAlign="center"
+          height="100%"
+          // border="1em solid rgb(254,253,251) "
+        >
+          <VStack gap="4">
+              <Heading color="red" font="playfair_display" weight="900" align="center" size="64">
+                REGEN 🍄 STATS
+              </Heading>
+              <Divider color="darkGrey" />
+              <Spacer size="22" />
+              <Box flexDirection="row" alignHorizontal="left" alignVertical="center">
                 {/* <Box 
                   borderStyle="solid" 
-                  borderRadius="8"
-                  borderWidth="2" 
-                  borderColor="red" 
-                  height="96" 
-                  width="560" 
-                /> */}
-                <Divider color="red" />
-                <Spacer size="22" />
-                <Text align="center" color="grey" size="14">
-                  Gift storage to the users you follow who are low on storage!
-                </Text> <Text align="center" color="grey" size="14">
-                  Powered by Arbitrum.
-                </Text>
-                <Spacer size="22" />
-                <Divider color="red" />
-                <Box flexDirection="row" justifyContent="center">
-                    <Text color="black" align="center" size="14">created by</Text>
-                    <Spacer size="10" />
-                    <Text color="grey" decoration="underline" align="center" size="14"> @0x94t3z</Text>
+                  borderRadius="42"
+                  borderWidth="4" 
+                  borderColor="blue" 
+                  height="64" 
+                  width="64" 
+                >
+                  <Image
+                    borderRadius="38"
+                    height="56"
+                    width="56"
+                    objectFit="cover"
+                    src={userData.pfp_url}
+                  />
+                </Box> */}
+
+                <img
+                    height="80"
+                    width="80"
+                    src='https://avatars.githubusercontent.com/u/52822242?v=4'
+                    style={{
+                      borderRadius: "0%",
+                      border: "3.5px solid #99A9B5",
+                    }}
+                  />
+                
+                <Spacer size="12" />
+                  <Box flexDirection="column" alignHorizontal="left">
+                    <Text color="black" align="left" size="24">
+                      0x94t3z
+                    </Text>
+                    <Text color="grey" align="left" size="16">
+                      @{'0x94t3z.eth'}
+                    </Text>
+                  </Box>
+              </Box>
+              <Spacer size="22" />
+              <Box
+                borderStyle="solid" 
+                borderRadius="8"
+                borderWidth="2" 
+                borderColor="darkGrey" 
+                padding="22"
+                height="96" 
+                width="100%" 
+              >
+                <Box flexDirection="row" gap="8">
+                  <Box flex="1" >
+                    <Text color="black" align="start" size="32">
+                      Allowance
+                    </Text>
+                  </Box>
+                  <Spacer size="256" />
+                  <Box flex="1" paddingRight="32">
+                    <Text color="darkGrey" align="end" size="32">
+                      42000
+                    </Text>
+                  </Box>
                 </Box>
-            </VStack>
-        </Box>
-      ),
-      intents: [
-        <TextInput placeholder="Search by username" />,
-        <Button action='/stats'>My Stats</Button>,
-        <Button value="oranges">Search</Button>,
-        <Button.Link href='https://warpcast.com/0x94t3z.eth'>Creator</Button.Link>,
-        <Button.Link href={CAST_INTENS}>Share</Button.Link>,
-      ],
-    })
-  } catch (error) {
-    return c.res({
-      image: (
-        <Box
-            grow
-            alignVertical="center"
-            backgroundColor="bg"
-            padding="48"
-            textAlign="center"
-            height="100%"
-        >
-            <VStack gap="4">
-                <Spacer size="16" />
-                <Heading color="red" weight="900" align="center" size="32">
-                  ⚠️ Failed ⚠️
-                </Heading>
-                <Spacer size="22" />
-                <Text align="center" color="black" size="16">
-                   Uh oh, something went wrong!
-                </Text>
-                <Spacer size="22" />
-                <Box flexDirection="row" justifyContent="center">
-                    <Text color="black" align="center" size="14">created by</Text>
-                    <Spacer size="10" />
-                    <Text color="grey" decoration="underline" align="center" size="14"> @0x94t3z</Text>
-                </Box>
-            </VStack>
-        </Box>
-      ),
-      intents: [
-        <Button.Reset>Try again</Button.Reset>,
-      ]
-    });
-  }
+              </Box>
+              <Spacer size="22" />
+              <Box
+                borderStyle="solid" 
+                borderRadius="8"
+                borderWidth="2" 
+                borderColor="darkGrey"
+                padding="22"
+                height="128" 
+                width="100%" 
+              >
+                    <Text color="black" align="center" size="32">
+                      Points
+                    </Text>
+            
+                    <Text color="darkGrey" align="center" size="32">
+                      28481
+                    </Text>
+         
+              </Box>
+              <Spacer size="22" />
+              <Box flexDirection="row" justifyContent="center">
+                  <Text color="black" align="center" size="14">created by</Text>
+                  <Spacer size="10" />
+                  <Text color="grey" decoration="underline" align="center" size="14"> @0x94t3z</Text>
+              </Box>
+          </VStack>
+      </Box>
+    ),
+    intents: [
+      <TextInput placeholder="Search by username" />,
+      <Button action='/stats'>My Stats</Button>,
+      <Button action='/search'>Search</Button>,
+      <Button.Link href='https://warpcast.com/0x94t3z.eth'>Creator</Button.Link>,
+      <Button.Link href={CAST_INTENS}>Share</Button.Link>,
+    ],
+  })
 })
+
 
 app.frame('/stats', async (c) => {
   const { fid } = c.var.interactor || {}
@@ -282,9 +298,18 @@ app.frame('/stats', async (c) => {
                   padding="22"
                   height="128" 
                   width="100%" 
-                ></Box>
+                >
+                  
+                    <Text color="black" align="center" size="32">
+                      Points
+                    </Text>
+            
+                    <Text color="darkGrey" align="center" size="32">
+                      28481
+                    </Text>
+
+                </Box>
                 <Spacer size="22" />
-                {/* <Divider color="red" /> */}
                 <Box flexDirection="row" justifyContent="center">
                     <Text color="black" align="center" size="14">created by</Text>
                     <Spacer size="10" />
