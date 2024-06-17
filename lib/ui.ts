740e364a59e117f998c0f6e778373d333fdbd2db
Muhamad Taopik <mr.94t3z@gmail.com>
2024-06-17 10:30:47 +0700

    🐸 refactor code v22

--- lib/ui.ts
+++ lib/ui.ts
@@ -2,16 +2,16 @@ import { createSystem } from "frog/ui";
 
 export const { Box, Row, Rows, Columns, Column, Divider, Image, Heading, Text, VStack, Spacer, vars } = createSystem({
   colors: {
-    bg: "rgb(254,253,251)",
-    white: "white",
-    black: "rgb(53,45,57)",
-    red: "rgb(243,3,62)",
-    tosca: "rgb(157,204,237)",
-    blue: "rgb(17,54,93)",
-    purple: "rgb(48,13,79)",
-    pink: "rgb(253,145,158)",
-    darkGrey: "rgb(153,169,181)",
-    grey: "rgba(128, 128, 128, 0.75)",
+    // bg: "rgb(254,253,251)",
+    // white: "white",
+    // black: "rgb(53,45,57)",
+    // red: "rgb(243,3,62)",
+    // tosca: "rgb(157,204,237)",
+    // blue: "rgb(17,54,93)",
+    // purple: "rgb(48,13,79)",
+    // pink: "rgb(253,145,158)",
+    // darkGrey: "rgb(153,169,181)",
+    // grey: "rgba(128, 128, 128, 0.75)",
   },
   frames: {
     height: 1024,
